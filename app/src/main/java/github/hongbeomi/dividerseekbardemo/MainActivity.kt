8bb63db88668200a5bec3faf6e0961f0774db1a3
hongbeomi <dks3963@naver.com>
2020-06-03 13:26:55 +0900

    test

--- app/src/main/java/github/hongbeomi/dividerseekbardemo/MainActivity.kt
+++ app/src/main/java/github/hongbeomi/dividerseekbardemo/MainActivity.kt
@@ -12,34 +12,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val dividerSeekBar = DividerSeekBar(this).apply {
+
+        }
+
         dividerSeekBar_test.apply {
-            max = 10
+            max = 100
+            setOffActivatedEvent()
+            setOnActivatedEvent()
             setTextLocationMode(DividerSeekBar.TEXT_LOCATION_BOTTOM)
-//            setTextInterval(10)
+            setTextInterval(10)
             setTextColor(R.color.black)
             setTextSize(R.dimen.sp_12)
-//            setSeaLineColor(R.color.light_blue_600)
+            setSeaLineColor(R.color.light_blue_600)
             setSeaLineStrokeWidth(R.dimen.dp_1)
-//            setDividerInterval(10)
-//            setDividerColor(R.color.light_blue_600)
+            setDividerInterval(10)
+            setDividerColor(R.color.light_blue_600)
             setDividerStrokeWidth(R.dimen.dp_1)
             setThumbDefaultDrawable(R.drawable.bg_thumb_default)
             setThumbActivatedDrawable(R.drawable.bg_thumb_activated)
             setActiveMode(DividerSeekBar.ACTIVE_MODE_MINIMUM)
-            setActivateTargetValue(3)
+            setActivateTargetValue(30)
             setOnDividerSeekBarChangeStateListener(
                 object : DividerSeekBar.OnDividerSeekBarChangeStateListener {
                     override fun onProgressEnabled(dividerSeekBar: DividerSeekBar, progress: Int) {
-//                        textView_test.apply {
-//                            text = "$progress :: ON!"
-//                            setTextColor(resources.getColor(R.color.light_blue_600))
-//                        }
+                        textView_test.apply {
+                            text = "$progress :: ON!"
+                            setTextColor(resources.getColor(R.color.light_blue_600))
+                        }
                     }
                     override fun onProgressDisabled(dividerSeekBar: DividerSeekBar, progress: Int) {
-//                        textView_test.apply {
-//                            text = "$progress :: OFF!"
-//                            setTextColor(resources.getColor(R.color.black))
-//                        }
+                        textView_test.apply {
+                            text = "$progress :: OFF!"
+                            setTextColor(resources.getColor(R.color.black))
+                        }
                     }
                 })
         }
